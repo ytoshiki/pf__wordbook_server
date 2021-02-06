5048dd0c57046fd5aadf3b56b63a7dd01e5343c0
Toshiki Yoshioka <hnkc.ysoktsk@gmail.com>
2021-02-06 22:23:33 +0900

    owlbot

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,17 +1,25 @@
+const { default: axios } = require('axios');
+
 const router = require('express').Router();
-const Owlbot = require('owlbot-js');
-const client = Owlbot(process.env.OWLBOT_TOKEN);
+// const Owlbot = require('owlbot-js');
+// const client = Owlbot(process.env.OWLBOT_TOKEN);
+// await client.define(word);
 
 router.get('/:word', async (req, res) => {
   const word = req.params.word;
-  console.log(process.env.OWLBOT_TOKEN);
-  console.log(word, client, Owlbot);
+
   try {
-    const result = await client.define(word);
-    if (!result) {
+    const result = await axios(`https://owlbot.info/api/v4/dictionary/${word}`, {
+      headers: {
+        Authorization: `Token ${process.env.OWLBOT_TOKEN}`
+      }
+    });
+
+    const data = await result.data;
+    if (!data) {
       throw new Error('Not Found');
     }
-    res.send(result);
+    res.send(data);
   } catch (error) {
     res.status(404).json({
       success: false,
