0e4573cf021e8cf6053d957acd63e7664766c37e
t-yoshioka-gg <h.yoshioka@grow-group.jp>
2023-01-22 17:10:27 +0900

    fix

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,32 +1,36 @@
-const { default: axios } = require('axios');
-const fetch = require('node-fetch');
+const { default: axios } = require("axios");
+const fetch = require("node-fetch");
 
-const router = require('express').Router();
+const router = require("express").Router();
 // const Owlbot = require('owlbot-js');
 // const client = Owlbot(process.env.OWLBOT_TOKEN);
 // await client.define(word);
 
-router.get('/:word', async (req, res) => {
+router.get("/:word", async (req, res) => {
   const word = req.params.word;
 
   try {
     // const result =
     fetch(`https://owlbot.info/api/v4/dictionary/${word}`, {
       headers: {
-        Authorization: `Token ${process.env.OWLBOT_TOKEN}`
-      }
+        Authorization: `Token ${process.env.OWLBOT_TOKEN}`,
+      },
     })
       .then((res) => res.json())
       .then((data) => {
         if (!data.definitions) {
-          throw new Error('Not Found');
+          throw new Error("Not Found");
         }
-        res.send(data);
+
+        res.status(200).json({
+          success: true,
+          data,
+        });
       })
       .catch((err) => {
         res.status(404).json({
           success: false,
-          message: 'Not Found'
+          message: "Not Found",
         });
       });
 
@@ -34,7 +38,7 @@ router.get('/:word', async (req, res) => {
   } catch (error) {
     res.status(404).json({
       success: false,
-      message: error.message || 'Not Found'
+      message: error.message || "Not Found",
     });
   }
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,54 +1,59 @@
-const router = require('express').Router();
-const UserSchema = require('../models/UserModel');
-const auth = require('../middleware/auth');
+const router = require("express").Router();
+const UserSchema = require("../models/UserModel");
+const auth = require("../middleware/auth");
 
-router.get('/', auth, (req, res) => {
+router.get("/", auth, (req, res) => {
   res.send({
     user: req.user,
-    token: req.token
+    token: req.token,
   });
 });
 
 // Signup
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   const reqUser = new UserSchema(req.body);
   try {
     const user = await reqUser.save();
-    const token = await user.generateToken()
+    const token = await user.generateToken();
 
     res.status(201).json({
       success: true,
       data: user,
-      token
+      token,
     });
   } catch (error) {
     res.status(400).json({
       success: false,
-      message: error.message
-    });;
+      message: error.message,
+    });
   }
 });
 
 // Signin
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   if (!email || !password) {
     return res.status(400).json({
       success: false,
-      message: 'Either email and password is missing'
+      message: "Either email and password is missing",
     });
   }
 
   try {
-    const user = await UserSchema.findOne({ email });
+    const user = await UserSchema.findOne({
+      email: email,
+    });
 
     if (!user) {
-      throw new Error('Email Not Found');
+      throw new Error("Email Not Found");
     }
 
-    const passwordMatch = await UserSchema.compareHashedPassword(password, user.password);
+    const passwordMatch = await UserSchema.compareHashedPassword(
+      password,
+      user.password
+    );
 
     if (!passwordMatch) {
       throw new Error("Password and Email don't match");
@@ -59,22 +64,22 @@ router.post('/signin', async (req, res) => {
     res.send({
       success: true,
       data: user,
-      token
+      token,
     });
   } catch (error) {
     console.log(error);
     res.status(500).json({
       success: false,
-      message: 'User Not Found'
+      message: "User Not Found",
     });
   }
 });
 
-router.post('/logout', auth, async (req, res) => {
+router.post("/logout", auth, async (req, res) => {
   await req.user.breakToken(req.token);
   res.send({
     success: true,
-    message: 'Logged Out Successfully'
+    message: "Logged Out Successfully",
   });
 });
 
